eb1b533e00b94f21a061c86c90e5985e6a1eda3b
erikdias7 <103780028+erikdias7@users.noreply.github.com>
2022-04-18 21:11:05 -0300

    Update system.py

--- system.py
+++ system.py
@@ -4,8 +4,8 @@ import sqlite3
 def start():
     con = sqlite3.connect('dados.db')
     cursor = con.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS motorista (id integer not null, nome VARCHAR(200), cpf VARCHAR(11), email VARCHAR(100), codigo_plano integer, PRIMARY KEY (id));")
-    cursor.execute("CREATE TABLE IF NOT EXISTS veiculos (codigo integer, nome_do_plano VARCHAR(200), valor_mensal real);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS motorista (reg integer not null, name VARCHAR(200), type_license VARCHAR(100), validity_license VARCHAR(100), PRIMARY KEY (reg));")
+    cursor.execute("CREATE TABLE IF NOT EXISTS veiculos (codigo integer, marca VARCHAR(200), modelo VARCHAR(200), data_fabricação VARCHAR(200), km_atual VARCHAR(200), ultima_km_manutencao VARCHAR(200));")
     cursor.execute("CREATE TABLE IF NOT EXISTS rotas (codigo integer, nome_do_plano VARCHAR(200), valor_mensal real);")
     cursor.execute("CREATE TABLE IF NOT EXISTS plano (codigo integer, nome_do_plano VARCHAR(200), valor_mensal real);")
     con.commit()
